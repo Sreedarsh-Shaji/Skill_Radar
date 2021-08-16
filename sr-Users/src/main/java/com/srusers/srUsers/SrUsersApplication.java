a1ff1a300e9a57a1e64c90c8f673281d244811a9
Sreedarsh S - hp <SreedarshShaji@outlook.com>
2021-08-17 01:36:53 +0530

    Added user micro service

---
+++ sr-Users/src/main/java/com/srusers/srUsers/SrUsersApplication.java
@@ -0,0 +1,13 @@
+package com.srusers.srUsers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrUsersApplication.class, args);
+	}
+
+}
---
+++ sr-Users/src/test/java/com/srusers/srUsers/SrUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srusers.srUsers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
