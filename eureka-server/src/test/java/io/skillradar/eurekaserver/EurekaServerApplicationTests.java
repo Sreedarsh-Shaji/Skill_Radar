09c12a48e2a100352ddcf1e6ce780180daefd94c
Sreedarsh S - hp <SreedarshShaji@outlook.com>
2021-08-23 21:22:27 +0530

    Eureka server added , errors involved

---
+++ eureka-server/src/test/java/io/skillradar/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.skillradar.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
