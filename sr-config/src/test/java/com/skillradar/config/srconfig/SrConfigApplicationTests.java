ae97714ed8f3c59493dbd9f9103ca2d9eff21605
Sreedarsh Shaji <tpzkollam@gmail.com>
2021-10-09 21:51:05 +0530

    Added the config server configuration

---
+++ sr-config/src/test/java/com/skillradar/config/srconfig/SrConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillradar.config.srconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
